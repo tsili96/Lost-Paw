bc0118e630cc9d76b8ec11b2266ca2efa65fc0ca
Lina Tsialouki <tsialoukilina@gmail.com>
2024-12-03 16:40:25 +0200

    wip

--- LostPaw/ViewModels/DisplayPostListViewModel.cs
+++ LostPaw/ViewModels/DisplayPostListViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace LostPaw.ViewModels
+﻿using LostPaw.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LostPaw.ViewModels
 {
     public class DisplayPostListViewModel
     {
+        public PostType Type { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateLostFound { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+
     }
 }
